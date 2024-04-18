6dde96f0fd9e3d8ea6eb5aa1f7be3047ea272663
Jessica Soares <jess.soares@hotmail.com>
2024-04-18 10:16:29 +0100

    Atualização

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,4 +7,34 @@
 
 import SwiftUI
 
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃" ,"🕷️", "🍭", "🕸️", "😱", "🙀", "💀", "☠️", "👿", "🧙🏻‍♀️", "👹"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            }else {
+                return "⁉️"
+            }
+        }
+    }
+        
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card){
+        model.choose(card)
+    }
+}
+
 
